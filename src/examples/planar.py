ab0bd70af88a5af35baec0b79a314e486fc0b5f1
letishnick <letishnick@gmail.com>
2020-10-12 14:19:31 +0200

    Updated an example

--- src/examples/planar.py
+++ src/examples/planar.py
@@ -1,34 +1,32 @@
 from simpleoptics.waveguide import Waveguide
 from simpleoptics.simulators.planar import *
 
-# dimensions
-# the planar waveguide doesn't have a withs, 
-# but it is set implicitly set nevertheless to 10 μm
+## Dimensions
+# the planar waveguide doesn't have a width, 
+# nevertheless it is implicitly set to 10 μm,
+# which can be changed if necessary
 
 height = 0.22              # [μm]
 
-# materials
-# can be defined as strings from the available ones in /epsilon_data/ folder
-# or for the consnant values 'n = value' or 'epsr = value'
- # if the substrate is not set it is implicitly equal to the cladding
+## Materials
+# can be defined as strings from the available ones in the /epsilon_data/
+# or in the form of 'n = value' or 'epsr = value'
+# if the substrate is not set it is implicitly equal to the cladding
 substrate = 'sio2'
 core =      'si'
 cladding =  'sio2'
 
-# substrate = 'n = 1.444'
-# core =    'n = 3.4'
-# cladding ='n = 1.444'
-
-# ranges
+## Calculation ranges
 # The choice is between wavelengths, frequency and V-number
-# with V-number variable materials are not possible (because the normalisation is impossible)
+# with V-number variable materials are not possible 
+# (because the normalisation is impossible)
 units = 'wavelength'       # [μm]
 start = 1.3
 stop = 1.9
-points = 11
+points = 101
 
 ## Setting the simulation
-# creating a waveguide
+# creating a waveguide (substrate can be simply omitted)
 wg1 = Waveguide(substrate=substrate, core=core, cladding=cladding,
                 height=height)
 
@@ -39,8 +37,10 @@ sim = Simulation_planar(waveguide=wg1,units=units,
 
 sim.simulate(wg1)
 
-# available parameters
+# available output parameters
 V = wg1.V
 beta = wg1.beta
 ng = wg1.ng
-neff = wg1.neff
+
+# all parameters can be called on a certain freq, wavelength or V
+neff_1550 = wg1.neff(wavelength=1.55)
